06a6c966c01cd99ec6116d62e56cd4aa73d888ff
Ivan Ilic <me@mrisaacs.org>
2022-04-14 10:48:11 +0200

    SW3CL-1 feat: add ogp and twitter meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,8 +5,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s by MrIsaacs',
-    title: 'nuxt-sw3cl',
+    titleTemplate: '%s - nuxt-sw3cl by MrIsaacs',
     htmlAttrs: {
       lang: 'en-US'
     },
@@ -15,9 +14,89 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'author', name: 'author', content: 'Ivan Ilić' },
       { hid: 'description', name: 'description', content: 'nuxt static web3 client' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
+      /**
+       * Open Graph Twitter Card
+       */
+      {
+        hid: 'twitter:card',
+        name: 'twitter:card',
+        content: 'summary'
+      },
+      {
+        hid: 'twitter:creator',
+        name: 'twitter:creator',
+        content: '@diamantberg'
+      },
+      {
+        hid: 'twitter:url',
+        name: 'twitter:url',
+        content: 'https://mrisaacs.org/nuxt-sw3cl'
+      },
+      {
+        hid: 'twitter:title',
+        name: 'twitter:title',
+        content: 'nuxt-sw3cl by MrIsaacs'
+      },
+      {
+        hid: 'twitter:description',
+        name: 'twitter:description',
+        content:
+          'nuxt-sw3cl is a full static web3 client'
+      },
+      {
+        hid: 'twitter:image',
+        name: 'twitter:image',
+        content: 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTI0IiBoZWlnaHQ9IjEyNCIgdmlld0JveD0iMCAwIDEyNCAxMjQiIGZpbGw9Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CjxwYXRoIGZpbGwtcnVsZT0iZXZlbm9kZCIgY2xpcC1ydWxlPSJldmVub2RkIiBkPSJNNTUuNzQ5OCAyNy4xNTUxQzUyLjUyNzcgMjEuNjE1IDQ0LjQ3MjMgMjEuNjE0OSA0MS4yNTAyIDI3LjE1NTFMNi4xMzQwNCA4Ny41MzQ2QzIuOTExOTEgOTMuMDc0OCA2LjkzOTU2IDEwMCAxMy4zODM4IDEwMEg0MC43OTc1QzM4LjA0MzggOTcuNTkzNCAzNy4wMjQxIDkzLjQzMDMgMzkuMTA3OSA4OS44NTg0TDY1LjcwMzMgNDQuMjY5NEw1NS43NDk4IDI3LjE1NTFaIiBmaWxsPSIjODBFRUMwIi8+CjxwYXRoIGQ9Ik03OC4wMDAyIDQwLjM5OTdDODAuNjY2OCAzNS44NjY4IDg3LjMzMzIgMzUuODY2OCA4OS45OTk4IDQwLjM5OTdMMTE5LjA2MSA4OS44MDFDMTIxLjcyOCA5NC4zMzM5IDExOC4zOTUgMTAwIDExMy4wNjIgMTAwSDU0LjkzODNDNDkuNjA1MiAxMDAgNDYuMjcxOSA5NC4zMzM5IDQ4LjkzODUgODkuODAxTDc4LjAwMDIgNDAuMzk5N1oiIGZpbGw9IiMwMERDODIiLz4KPC9zdmc+Cg=='
+      },
+      {
+        hid: 'twitter:image:alt',
+        name: 'twitter:image:alt',
+        content: 'Nuxt logo'
+      },
+      /**
+       * Open Graph Facebook Card
+       */
+      { hid: 'og:site_name', property: 'og:site_name', content: 'MrIsaacs' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      {
+        hid: 'og:url',
+        property: 'og:url',
+        content: 'https://mrisaacs.org/nuxt-sw3cl'
+      },
+      {
+        hid: 'og:title',
+        property: 'og:title',
+        content: 'nuxt-sw3cl'
+      },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content:
+          'nuxt-sw3cl is a full static web3 client'
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTI0IiBoZWlnaHQ9IjEyNCIgdmlld0JveD0iMCAwIDEyNCAxMjQiIGZpbGw9Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CjxwYXRoIGZpbGwtcnVsZT0iZXZlbm9kZCIgY2xpcC1ydWxlPSJldmVub2RkIiBkPSJNNTUuNzQ5OCAyNy4xNTUxQzUyLjUyNzcgMjEuNjE1IDQ0LjQ3MjMgMjEuNjE0OSA0MS4yNTAyIDI3LjE1NTFMNi4xMzQwNCA4Ny41MzQ2QzIuOTExOTEgOTMuMDc0OCA2LjkzOTU2IDEwMCAxMy4zODM4IDEwMEg0MC43OTc1QzM4LjA0MzggOTcuNTkzNCAzNy4wMjQxIDkzLjQzMDMgMzkuMTA3OSA4OS44NTg0TDY1LjcwMzMgNDQuMjY5NEw1NS43NDk4IDI3LjE1NTFaIiBmaWxsPSIjODBFRUMwIi8+CjxwYXRoIGQ9Ik03OC4wMDAyIDQwLjM5OTdDODAuNjY2OCAzNS44NjY4IDg3LjMzMzIgMzUuODY2OCA4OS45OTk4IDQwLjM5OTdMMTE5LjA2MSA4OS44MDFDMTIxLjcyOCA5NC4zMzM5IDExOC4zOTUgMTAwIDExMy4wNjIgMTAwSDU0LjkzODNDNDkuNjA1MiAxMDAgNDYuMjcxOSA5NC4zMzM5IDQ4LjkzODUgODkuODAxTDc4LjAwMDIgNDAuMzk5N1oiIGZpbGw9IiMwMERDODIiLz4KPC9zdmc+Cg=='
+      },
+      {
+        hid: 'og:image:secure_url',
+        property: 'og:image:secure_url',
+        content: 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTI0IiBoZWlnaHQ9IjEyNCIgdmlld0JveD0iMCAwIDEyNCAxMjQiIGZpbGw9Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CjxwYXRoIGZpbGwtcnVsZT0iZXZlbm9kZCIgY2xpcC1ydWxlPSJldmVub2RkIiBkPSJNNTUuNzQ5OCAyNy4xNTUxQzUyLjUyNzcgMjEuNjE1IDQ0LjQ3MjMgMjEuNjE0OSA0MS4yNTAyIDI3LjE1NTFMNi4xMzQwNCA4Ny41MzQ2QzIuOTExOTEgOTMuMDc0OCA2LjkzOTU2IDEwMCAxMy4zODM4IDEwMEg0MC43OTc1QzM4LjA0MzggOTcuNTkzNCAzNy4wMjQxIDkzLjQzMDMgMzkuMTA3OSA4OS44NTg0TDY1LjcwMzMgNDQuMjY5NEw1NS43NDk4IDI3LjE1NTFaIiBmaWxsPSIjODBFRUMwIi8+CjxwYXRoIGQ9Ik03OC4wMDAyIDQwLjM5OTdDODAuNjY2OCAzNS44NjY4IDg3LjMzMzIgMzUuODY2OCA4OS45OTk4IDQwLjM5OTdMMTE5LjA2MSA4OS44MDFDMTIxLjcyOCA5NC4zMzM5IDExOC4zOTUgMTAwIDExMy4wNjIgMTAwSDU0LjkzODNDNDkuNjA1MiAxMDAgNDYuMjcxOSA5NC4zMzM5IDQ4LjkzODUgODkuODAxTDc4LjAwMDIgNDAuMzk5N1oiIGZpbGw9IiMwMERDODIiLz4KPC9zdmc+Cg=='
+      },
+      {
+        hid: 'og:image:alt',
+        property: 'og:image:alt',
+        content: 'Nuxt logo'
+      }
     ],
     link: [
+      {
+        hid: 'canonical',
+        rel: 'canonical',
+        href: `https://mrisaacs.org/nuxt-sw3cl/`
+      },
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
@@ -35,6 +114,9 @@ export default {
   plugins: [
     '~/plugins/nuxt-init.client.js'
   ],
+
+  // Auto import components, see https://go.nuxtjs.dev/config-components
+  components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="main-article two-thirds column">
+    <SocialHead
+      :title="page.title"
+      :description="page.description"
+      :image="'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTI0IiBoZWlnaHQ9IjEyNCIgdmlld0JveD0iMCAwIDEyNCAxMjQiIGZpbGw9Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CjxwYXRoIGZpbGwtcnVsZT0iZXZlbm9kZCIgY2xpcC1ydWxlPSJldmVub2RkIiBkPSJNNTUuNzQ5OCAyNy4xNTUxQzUyLjUyNzcgMjEuNjE1IDQ0LjQ3MjMgMjEuNjE0OSA0MS4yNTAyIDI3LjE1NTFMNi4xMzQwNCA4Ny41MzQ2QzIuOTExOTEgOTMuMDc0OCA2LjkzOTU2IDEwMCAxMy4zODM4IDEwMEg0MC43OTc1QzM4LjA0MzggOTcuNTkzNCAzNy4wMjQxIDkzLjQzMDMgMzkuMTA3OSA4OS44NTg0TDY1LjcwMzMgNDQuMjY5NEw1NS43NDk4IDI3LjE1NTFaIiBmaWxsPSIjODBFRUMwIi8+CjxwYXRoIGQ9Ik03OC4wMDAyIDQwLjM5OTdDODAuNjY2OCAzNS44NjY4IDg3LjMzMzIgMzUuODY2OCA4OS45OTk4IDQwLjM5OTdMMTE5LjA2MSA4OS44MDFDMTIxLjcyOCA5NC4zMzM5IDExOC4zOTUgMTAwIDExMy4wNjIgMTAwSDU0LjkzODNDNDkuNjA1MiAxMDAgNDYuMjcxOSA5NC4zMzM5IDQ4LjkzODUgODkuODAxTDc4LjAwMDIgNDAuMzk5N1oiIGZpbGw9IiMwMERDODIiLz4KPC9zdmc+Cg=='"
+    />
     <div class="article-layer">
       <h1 id="main-title">
         <p>{{ page.title }}</p>
@@ -15,10 +20,23 @@
 </template>
 
 <script>
+import SocialHead from '~/components/SocialHead.vue'
+
 export default {
+  components: {
+    SocialHead
+  },
   async asyncData ({ $content }) {
     const page = await $content('README').fetch()
     return { page }
+  },
+  head () {
+    return {
+      title: this.page.title,
+      meta: [
+        { hid: 'description', name: 'description', content: this.page.description }
+      ]
+    }
   }
 }
 </script>
